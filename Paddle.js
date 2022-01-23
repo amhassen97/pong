e831cad7f1c4d6814fe73ad6e29d0fba8cf89213
Abdullah Hassen <am.hassen977@gmail.com>
2022-01-23 21:23:02 +0200

    got ball moving, busy with paddles

---
+++ Paddle.js
@@ -0,0 +1,16 @@
+export default class Paddle {
+    constructor(paddleElem){
+        this.paddleElem = paddleElem;
+    }
+
+
+    get position(){
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"));
+
+    }
+
+    set position(value){
+        this.paddleElem.style.setProperty("--position", value);
+
+    }
+}
