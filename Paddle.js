d07ec119b97d0c9e212b0da73bcbe567a2090712
Abdullah Hassen <am.hassen977@gmail.com>
2022-01-23 23:01:04 +0200

    pong game working

--- Paddle.js
+++ Paddle.js
@@ -1,3 +1,6 @@
+const SPEED = .02;
+
+
 export default class Paddle {
     constructor(paddleElem){
         this.paddleElem = paddleElem;
@@ -12,5 +15,17 @@ export default class Paddle {
     set position(value){
         this.paddleElem.style.setProperty("--position", value);
 
+    }
+
+    rect(){
+        return this.paddleElem.getBoundingClientRect();
+    }
+
+    reset(){
+        this.position = 50;
+    }
+
+    update(delta, ballHeight){
+        this.position += SPEED * delta * (ballHeight - this.position);
     }
 }
